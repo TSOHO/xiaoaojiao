bb3b5d02fc04b7aeb34fc4f4d9a983995b6c328a
hyingzi <h.yingzi@gmail.com>
2017-10-17 14:04:48 +0800

    test

--- util/github.js
+++ util/github.js
@@ -1,14 +1,22 @@
 const Axios = require('axios')
 
+const path = require('path')
+
 const organization = 'TSOHO'
   /**
    * 获取organization所有模板
    */
 module.exports = {
-  getTemplateList: () => {
+  getTemplateList: function () {
     return Axios.get(`https://api.github.com/users/${organization}/repos`)
   },
-  getPackage: (name) => {
+  getPackage: function (name) {
     return Axios.get(`https://raw.githubusercontent.com/TSOHO/template-${name}/master/package.json`)
+  },
+  localTemplate: function (name) {
+    return path.join(__dirname, '../template', 'template-' + name + '-master')
+  },
+  localPackage: function (name) {
+    return path.join(__dirname, '../template', 'template-' + name + '-master/package.json')
   }
 }
